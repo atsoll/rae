6b131652bd7d609a00963c14acddfc5c17cfd9f5
atsoll <sollazzo@ualberta.ca>
2025-12-03 13:24:25 +0000

    fix for gh pages deployment

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-
+//to make gh pages happy
+var pref ="/rae/"
 
 var app = angular.module('template',[ 'ngSanitize', 'ngAnimate']);
 
@@ -10,7 +11,7 @@ app.controller('ctrl', function($scope, $window, $http) {
 
   $scope.model = {data:[{name:"Paradigm", curr:null, icon:"fa-brain", colour:"#1f77b4"}, {name:"Methodology", curr:null, icon:"fa-book", colour:"#ff7f0e"}, {name:"Method", curr:null, icon:"fa-flask", colour:"#2ca02c"}, {name:"Outcome", curr:null, icon:"fa-poll", colour:"#9467bd"}], overlay:false};
   for(let i=0;i<$scope.model.data.length; i++) {
-    $http.get("../data/" + $scope.model.data[i]["name"].toLowerCase() + ".csv").then(function(d){
+    $http.get(pref + "data/" + $scope.model.data[i]["name"].toLowerCase() + ".csv").then(function(d){
       let lines = d["data"].split("\n")
       lines.pop()
       let res = lines.map(function(l) {
